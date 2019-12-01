acd393cbc7900e6e8b40473eb50e4386ec3282cc
Fer Benavides <nockzblack@gmail.com>
2019-12-01 11:32:49 -0600

    Nodo struct and walk pointer funtion for Persona made
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- Persona.go
+++ Persona.go
@@ -4,16 +4,28 @@ import (
 	"fmt"
 )
 
+
+// Nodo is the class for the path/map
+type Nodo struct {
+	//person Persona
+
+}
 // Persona is defined
 type Persona struct {
 	speed int
 	id int
+	alivee bool
+	currentNode *Nodo
+}
+
+func (p *Persona) walk(pos *Nodo) {
+	p.currentNode = pos
 
 }
 
 
 func main() {
-	p := Persona{5, 01}
+	p := Persona{5, 1, true, nil}
 	fmt.Println("Hello Go")
 	fmt.Println(p.speed)
 }
