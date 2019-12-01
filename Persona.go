c8f3318ff462edd9739ce71934edb003915a3297
Fer Benavides <nockzblack@gmail.com>
2019-12-01 12:07:40 -0600

    People moving on goroutines and wait for all to finish with sync.WaitGroup object
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- Persona.go
+++ Persona.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"sync"
 )
 
 
@@ -18,14 +20,30 @@ type Persona struct {
 	currentNode *Nodo
 }
 
-func (p *Persona) walk(pos *Nodo) {
-	p.currentNode = pos
+func (p *Persona) walk(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Person %d has started to move\n", p.id)
+
+	time.Sleep(time.Duration(p.speed) * time.Second)
+	fmt.Printf("Person %d just moved to this plase\n", p.id)
+	//fmt.Println("Took this spot")
+	//fmt.Printf("Person %d  just realeased this place\n", p.id)
 
 }
 
 
 func main() {
-	p := Persona{5, 1, true, nil}
+	//p := Persona{5, 1, true, nil}
 	fmt.Println("Hello Go")
-	fmt.Println(p.speed)
+	//fmt.Println(p.speed)
+
+	var wg sync.WaitGroup
+	for index := 1; index < 11; index++ {
+		p := Persona{index,index,true, nil}
+		go p.walk(&wg)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+
 }
