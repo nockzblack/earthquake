a0004c36a7edfe8f2060b93b57c8fd768224ffd5
Fer Benavides <nockzblack@gmail.com>
2019-12-01 14:11:56 -0600

    initializing node object
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- Persona.go
+++ Persona.go
@@ -2,28 +2,60 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"sync"
+	"time"
 )
 
-
-// Nodo is the class for the path/map
-type Nodo struct {
-	//person Persona
-
+// Node is the class for the path/map
+type Node struct {
+	left         *Node
+	right        *Node
+	back         *Node
+	front        *Node
+	nextHop      *Node
+	isExit       bool
+	isBoder      bool
+	stepsToExtit int
+	mux          *sync.Mutex
 }
+
+func newNode(exit bool, border bool, steps int) *Node {
+	node := &Node{}
+	node.left = nil
+	node.right = nil
+	node.back = nil
+	node.front = nil
+	node.nextHop = nil
+	node.isExit = exit
+	node.isBoder = border
+	node.stepsToExtit = steps
+	node.mux = new(sync.Mutex)
+	return node
+}
+
 // Persona is defined
 type Persona struct {
-	speed int
-	id int
+	speed  int
+	id     int
 	alivee bool
-	currentNode *Nodo
+	pos    *Node
+}
+
+func (p *Persona) setPos(node *Node, wg *sync.WaitGroup) {
+	p.pos = node
+	fmt.Printf("In setPos from Persona %d\n", p.id)
+	p.pos.mux.Lock()
+	fmt.Printf("Person %d has locked the node\n", p.id)
+	time.Sleep(2 * time.Second)
+	defer p.pos.mux.Unlock()
+	fmt.Printf("Person %d has unlocked the node\n", p.id)
+	defer wg.Done()
 }
 
 func (p *Persona) walk(wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Printf("Person %d has started to move\n", p.id)
-
+	//p.pos.nextHop
 	time.Sleep(time.Duration(p.speed) * time.Second)
 	fmt.Printf("Person %d just moved to this plase\n", p.id)
 	//fmt.Println("Took this spot")
@@ -31,16 +63,19 @@ func (p *Persona) walk(wg *sync.WaitGroup) {
 
 }
 
-
 func main() {
 	//p := Persona{5, 1, true, nil}
 	fmt.Println("Hello Go")
 	//fmt.Println(p.speed)
 
 	var wg sync.WaitGroup
-	for index := 1; index < 11; index++ {
-		p := Persona{index,index,true, nil}
-		go p.walk(&wg)
+	n := newNode(false, false, 10)
+
+	for index := 1; index < 10; index++ {
+		p := Persona{index, index, true, nil}
+		fmt.Printf("Person %d has been created\n", index)
+		go p.setPos(n, &wg)
+		//go p.walk(&wg)
 		wg.Add(1)
 	}
 
